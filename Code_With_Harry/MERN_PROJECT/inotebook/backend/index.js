be601aa30e438fed3625703b6aca1c2bfd565ce6
git-groshan <roshangupta2020@gmail.com>
2024-06-22 18:32:42 +0530

    DB Router done for DB connection

---
+++ Code_With_Harry/MERN_PROJECT/inotebook/backend/index.js
@@ -0,0 +1,19 @@
+const connectToMongo = require('./dbConnect');
+const express = require('express')
+
+connectToMongo();
+
+const app = express();
+const port = 3000;
+
+//Available routes
+app.get('/' , (req,res)=>{
+    res.send("Hello Harry");
+});
+app.use('/api/auth' , require('./routes/auth'))
+app.use('/api/notes' , require('./routes/notes'))
+
+
+app.listen(port , ()=>{
+console.log("App is listening at port" , port);
+});
---
+++ Code_With_Harry/MERN_PROJECT/inotebook/backend/models/Notes.js
@@ -0,0 +1,26 @@
+const { type } = require('@testing-library/user-event/dist/type');
+const mongoose = require('mongoose');
+const connectToMongo = require('../dbConnect');
+
+
+const NotesSchema = new mongoose.Schema({
+    title:{
+        type:String,
+        required: true
+    },
+    description:{
+        type:String,
+        required: true,
+        unique: true
+    },
+    tag:{
+        type:String,
+        default: "General"
+    },
+    date:{
+        type: Date,
+        default: Date.now
+    },
+});
+
+module.exports = mongoose.model('Notes' , NotesSchema);
